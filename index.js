49c29211886c5a52a0e7213c36b9af2bcef990c8
amchikdaniel <omarbekovdunk@gmail.com>
2023-11-20 12:02:50 +0300

    bivaet.js to buttons.js New Buttons interaction.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -36,4 +39,4 @@ for (const file of eventFiles) {
     }
 }
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
